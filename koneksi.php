15570e9ebd38625f55b852738eacb825d2a255aa
SilviaNurMahmudah <72615487+SilviaNurMahmudah@users.noreply.github.com>
2021-10-10 13:23:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","voting");
+$koneksi = mysqli_connect("10.0.0.79","admin","Admin.123","evote");
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
